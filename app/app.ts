9a544a242dcc48e8f934b709e1e53644de46199d
jaum1234 <joaovitorsouzacoura@gmail.com>
2021-12-05 20:55:03 -0300

    tsconfig

--- app/app.ts
+++ app/app.ts
@@ -1,6 +1,7 @@
 import Negociacao from "./models/Negociacao.js";
 
 const negociacao = new Negociacao(new Date, '10', 100);
+
 console.log(negociacao.data);
 console.log(negociacao.quantidade);
 console.log(negociacao.volume);
